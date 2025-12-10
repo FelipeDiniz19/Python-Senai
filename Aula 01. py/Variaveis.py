93f3f9eefa8486e26d7c3f44a5416f57185527f7
Felipe Diniz <felipeborgesdiniz@gmail.com>
2025-12-10 11:35:40 -0300

    Aula 3

---
+++ Aula 01. py/Variaveis.py
@@ -0,0 +1,68 @@
+# Criando variável: 
+# Para criar uma variável devemos dar um nome para ela e passar para ela o que aquela variável corresponde, 
+# diferente de outras linguagens de programação no Python não é necessário passar o tipo de dados.
+# ATENÇÃO!
+# Caso sua variável tenha mais de duas palavras devemos seguir o padrão convencional utilizado pelos programadores, chamado de snake case.
+
+felipe_diniz = 18
+
+# Tipos primitivos:
+# - Tipos primitivos (int, flot, bool e str)
+# - função type()
+
+# INT()
+# int() (inteiro) é um tipo composto por caracteres numéricos (algorismo), utilizado para representar um número que pod ser escrito sem um componente decimal, 
+# podendo ter ou não sinal, isto é, ser positivo ou negativo. Os valores do tipo inteiro podem ser um tamanho ilimitado
+
+# Exemplos INT
+
+#Float()
+# O float ()(flutuante) também é composto por caracteres numéricos, mas é uilizado para representar múmeros reciomais (numénuros que podem ser escritos como fraçoes)
+#também conhecido como flutuante. Assim como os inteiros, podem ter sinal ou não e possuem um tamanho ilimitado. As casas decimais são separadas da porção inteira por um ponto(.)
+
+#exemplos de FLOAT()
+
+
+#BOLL()
+# O boll() é um tipo de dado lógico que pode assumir dois valores: falso ou verdadeiro (False ou True, respectivamente, em Python). 
+# Na lógica computacional, podem se considerados como 0 (falso) ou 1 (verdadeiro)
+
+#Str()
+# O str()(abriviação de string, que em portugues podemos traduzir como cadeia de caracteres)
+#  é uma sequencia/cadeia de caracteres utilizada para representar palavras, frases ou texto. As strings podem ser declaradas de diferentes formas, por meio de aspas simples('),
+#  aspas duplas(") ou triplas("')- três aspas simples consecutivas.
+
+#Função Type()
+# O python possui diferentes tipos de dados que classificam a categoria da informação que está sendo manipulada no código. 
+# Entretanto, podem existir situações em que precisaremos descobrir qual é o tipo de um determinado valor.
+
+#Operadores Aritméticos
+#- Simbolos
+#- Como utilizar
+
+# O Python possui seta operadores arimetricos que são utilizados para realizar operações matemáticas como adição, subtração e multiplicação . São elas:
+
+# + Adição                          soma os dois valores                                                                                   X + Y
+# - Subtração                       Subtrai o valor da esquerda para a direita                                                             X - Y
+# * Multiplicação                   Multilplica os dois valores                                                                            X * Y
+# / Divisão                         Divide o valor da esquerda pelo valor da direita. O resultado sempre retorna como Float                X / Y
+# % Módulo                          Resto da divisão do valor da esquerda pelo o da direita. o resultado sempre retorna int                X % Y
+# // Divisão inteira                Divisão inteira do valor da esquerda pelo valor da direita. O resultado sempre int                     X // Y
+# ** Exponenciação                  Realiza a pontencialização do valor da esquerda, elevando ele ao valor da direita                      X ** Y   
+
+# Cada operador tem sua utilzação dentro do necessario, no Python quando for calcular algum valor ele usa a convenção tradicional da matemática muito conhecido como PEMDAS
+# LETRA:                 SIGNIFICADO:
+# P                     Parenteses
+# E                     Exponenciação
+# M                     Multiplicação
+# D                     Divisão
+# A                     Adição
+
+# Entrada de informação
+# No Python conseguimos colocar informações em nossa variável sem que sja necessario a utilização daquela informação já pronta, 
+# com a utiização da função input() conseguimos fazer nosso usuário possa entrar com a informação e assim podemos tratar ela como acharmos necessario.
+
+#Comentarios
+# comentarios são trechos de códigos que são ignorados pelo interpretador a linguagem. 
+# O uso de comentarios auxilia na documentação do programa, pois fornece alumas informações e/ou orientação sobre o seu funcioanamento.
+# Em Python os comentarios, são identificados por um sinal de cerquilha (#), e qualquer conteúdo após este símbolo é ignorado pelo interprtador. 
